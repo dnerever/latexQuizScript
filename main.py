d99b3e57dbcfccbb46d427a01b164aef239b966c
Keith Bates <bateskeith@pm.me>
2023-11-21 11:01:08 -0700

    Saving current work on getCode() and waitForTexFile()

--- main.py
+++ main.py
@@ -12,14 +12,19 @@ def pdfToTxt(pdfFileName):
     firstPage = reader.pages[0]
     return firstPage.extract_text()
 
-def findRecentFile():
-    fileList = glob.glob('./*')
+def findRecentFile(file_type):
+    fileList = glob.glob("./*" + file_type)
     newestFile = max(fileList, key=os.path.getctime)
     return newestFile[2:]
 
+def fileCount(file_type):
+    fileList = glob.glob("./*" + file_type)
+    # newestFile = max(fileList, key=os.path.getctime)
+    return len(fileList)
+
 def confirmChanges():
     new_content = ""
-    with open(findRecentFile()) as file:
+    with open(findRecentFile(".pdf")) as file:
         for line in file:
             new_content += line
     file.close()
@@ -32,21 +37,33 @@ def confirmChanges():
 
 def waitForTexFile(name, idNum):
     t_end = time.time() + 60 * 5    # Time will run for 5 minutes
+    initial_file_count = len(glob.glob("./*.tex"))
     while time.time() < t_end:
-        if (file:=findRecentFile()) != recentFile:      # Cool way to save a value being checked in the if statement.   varName := captureVariable ;  https://stackoverflow.com/questions/1550134/how-to-assign-a-variable-in-an-if-condition-and-then-return-it
+        if (current_file_count:=fileCount(".tex")) != initial_file_count:      # Cool way to save a value being checked in the if statement.   varName := captureVariable ;  https://stackoverflow.com/questions/1550134/how-to-assign-a-variable-in-an-if-condition-and-then-return-it
+            file = findRecentFile(".tex")
             time.sleep(1)                               # Sleeping here seems to fix reliability when downloading from Canvas.
             sysCmd = "bash autoCompleteInfo '%s' '%s' '%s' " % (str(file), name, idNum)
             try:
                 os.system(sysCmd)
-                print("File" + str(file) + " changed where applicable")
+                print("---file '" + str(file) + "' changed where applicable---")
+                print("current: " + str(current_file_count) + ", previous: " + str(initial_file_count))
                 return
             except:
-                print("!!Error when trying to make changes!!")
+                print("-!-Error trying to make changes to '" + str(file) + "'-!-\n")
+                return
+
+def getCode():
+    content = pdfToTxt(recentFile)
+    regex = r'(?:Quiz Code \(enter in Canvas to get access to the LaTeX template\)[ .]*)([A-Z]\w+)'
+    try:
+        code = re.search(regex, content).group(1)
+    except:
+        print("-!-Failed to locate quiz code-!-")
 
 try:                                                    # Extract the quiz code from the most recently downloaded file (soon to be only PDF captured) and then copied to clipboard (Linux only) 
-    recentFile = findRecentFile()                       #finds most recently modified file in current directory
+    recentFile = findRecentFile(".pdf")                       #finds most recently modified file in current directory
     if(recentFile[-4:] != ".pdf"):
-        print("!!The most recent file is of the wrong type. PDF required!!\n Retrived file: " + recentFile)
+        print("-!-The most recent file '" + recentFile + "' is not a '.pdf'-!-\n")
         exit
     else:
         try:
@@ -58,17 +75,17 @@ try:                                                    # Extract the quiz code 
                     pc.copy(code)
                     print('Copied:' + code + ' to clipboard from ' + recentFile)
                 except:
-                    print("!!Code failed to copy to clipboard!!")
+                    print("-!-Code failed to copy to clipboard-!-")
                     print("Code: " + code + "\n")
             except:
-                print("!!Failed to locate quiz code!!")
+                print("-!-Failed to locate quiz code-!-")
         except:
-            print("!!Failed to read/open PDF!!")
+            print("-!-Failed to read/open PDF-!-")
 except:
-    print("!!Most recent file not found!!")
+    print("-!-Most recent file not found-!-")
 finally:
     try:                                                # The next file that will be added to the directory is the .tex file, which can have some info be auto completed including: (name, Student ID, Agreement)
         waitForTexFile(info.name, info.idNum)
-        confirmChanges()    # 
+        confirmChanges()
     except:
-        print("!!Failed to fill out your info!!")
+        print("-!-Failed to fill out your info-!-")
